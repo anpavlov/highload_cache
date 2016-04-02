46ab03571045b48859012f9dd3d7ccaeba0cbd72
alrogovsky <alex@rogovsky.ru>
2016-04-02 13:57:25 +0300

    MUCH REFACTOR

--- main.c
+++ main.c
@@ -3,104 +3,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PAD 1
-#define ADDED 100
-#define ITERS 100
-
 #define repeat(X) X; X;
 
 #define KB 1024
 #define MB 1024 * KB
-#define SIZE 0x1000000
-#define SPACE 101
-#define STEPS 1000
-//#define MAX_ITERS SIZE / SPACE
 
 typedef struct elem {
     struct elem *next;
-    int pad[2];
+    int a;
 } elem;
 
 //void add_to(elem *first, int num);
 elem *shuffle_loop(elem *first);
+elem *normal_loop(elem *first);
 elem *generate_list(int amt);
 void free_list(elem *first);
 
-volatile char arr[SIZE];
-int access[0xFFFFFF];
-
 int main() {
-
-
+    
     srand(time(NULL));
-
-//    ==== OLD ====
-//    elem *list = (elem*)malloc(sizeof(elem));
-//    list->next = NULL;
-//    =============
-
-
-//    ==== OLD ====
-
-    int max = 8 * 1024 * 1024 / sizeof(elem);
-
+    
+    int max = 256 * MB / sizeof(elem);
+    
     clock_t measured;
-    int i, count = 100;
-    for (count = 10; count < max;) {
-
-        if ((int)(count * sizeof(elem)) > 1024 * 1024) {
-            count += count/10;
-        } else if ((int)(count * sizeof(elem)) < 100 * 1024) {
-            count += 10;
-        } else {
-            count += 1000;
-        }
-
+    int count = 0;
+    for (count = 1; count < max;) {
+        
         elem *first_elem = generate_list(count);
-//        add_to(list, ADDED);
-
-        elem *tail = shuffle_loop(first_elem);
+//        elem *tail = shuffle_loop(first_elem);
+        elem *tail = normal_loop(first_elem);
+        
         volatile elem *t = first_elem;
-        volatile elem t1;
-//       (t = t->next))))))))))))))))))));
-
-        int j, k;
+        
+        int k;
         measured = clock();
-        for (k = 0; k < 50; ++k) {
-//            for (j = 0; j < count / 100; ++j) {
-                repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(t = t->next))))))))));
-//            }
+        for (k = 0; k < 5000; ++k) {
+            repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(t = t->next))))))))));
         }
-//        repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(repeat(t = t->next))))))))));
+        
         measured = clock() - measured;
-        printf("%d %d\n", (int)(((float)measured) * 1000000000/CLOCKS_PER_SEC)/(50 * 1024), (int)(count * sizeof(elem)));
+        
+        printf("%d;%d\n", (int)(((float)measured) * 1000000000 / CLOCKS_PER_SEC)/(5000 * 1024), (int)(count * sizeof(elem)));
+        
         tail->next = NULL;
         free_list(first_elem);
-
+        
+        if (count * sizeof(elem) > 32000 && count * sizeof(elem) < 256000){
+            count += 1280;
+        } else {
+            count *= 2;
+        }
+        
     }
-
-//    ================
-
-
-//    add_to(list, 9);
-//    shuffle_loop(list);
-
-//    int i = 0;
-//    repeat(repeat(i++));
-
+    
     return 0;
 }
 
 elem *shuffle_loop(elem *first) {
-
-//    count length
+    
+    //    count length
     int length = 1;
     elem *temp = first;
     while (temp->next) {
         ++length;
         temp = temp->next;
     }
-
+    
     elem **array = (elem**)malloc(sizeof(elem*) * length);
     int i;
     temp = first;
@@ -108,40 +76,34 @@ elem *shuffle_loop(elem *first) {
         array[i] = temp;
         temp = temp->next;
     }
-
+    
     for (i = 0; i < length / 2; i++) {
         int j = i + rand() / (RAND_MAX / (length - i) + 1);
         elem *t = array[j];
         array[j] = array[i];
         array[i] = t;
     }
-
+    
     temp = array[0];
     for (i = 0; i < length - 1; ++i) {
         temp->next = array[i + 1];
         temp = temp->next;
     }
     temp->next = array[0];
-
+    
     free(array);
-
     return temp;
 }
 
-//void add_to(elem *first, int num) {
-////    find tail
-//    while (first->next) {
-//        first = first->next;
-//    }
-//
-//    int i;
-//    for (i = 0; i < num; ++i) {
-//        elem *new_elem = (elem*)malloc(sizeof(elem));
-//        new_elem->next = NULL;
-//        first->next = new_elem;
-//        first = new_elem;
-//    }
-//}
+elem *normal_loop(elem *first){
+    elem* temp;
+    temp = first;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = first;
+    return temp;
+}
 
 elem *generate_list(int amt) {
     elem *list = (elem*)malloc(sizeof(elem));
@@ -149,8 +111,7 @@ elem *generate_list(int amt) {
     int i;
     for (i = 0; i < amt - 1; ++i) {
         elem *new_elem = (elem*)malloc(sizeof(elem));
-        new_elem->pad[0] = rand();
-        new_elem->pad[1] = rand();
+        new_elem->a = rand();
         list->next = new_elem;
         list = new_elem;
     }
@@ -158,8 +119,9 @@ elem *generate_list(int amt) {
     return first_elem;
 }
 
-void free_list(elem *first) {
 
+void free_list(elem *first) {
+    
     elem *temp = first;
     elem *temp1;
     while (temp->next) {
